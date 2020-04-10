f499ceb75674357b2214f589ccb3a55ab807d71c
urbanizzz <yuraoskin@gmail.com>
2020-04-11 02:23:27 +0700

    Simple request using tor proxy
    Add HTTP tunnel in tor
    Now address proxy is 127.0.0.1:9041

--- src/Main.hs
+++ src/Main.hs
@@ -3,16 +3,21 @@
 module Main where
 
 import              Network.HTTP.Simple
-import qualified    Data.ByteString.Char8           as BS
+import qualified    Data.ByteString.Lazy.Char8      as L8
 
 botURL = "https://api.telegram.org/bot949284451:AAGK8fCgIhv2KLcmT8Mz_bf-3hAl0Ccp7pA/getUpdates"
 nnn = "http://httpbin.org/get"
 
+{-
+working proxy
+"45.151.169.28" 80
+-}
 
-fetchJSON :: IO BS.ByteString
+
+fetchJSON :: IO L8.ByteString
 fetchJSON = do
-    let request = setRequestProxy (Just (Proxy "127.0.0.1" 8118)) botURL
-    res <- httpBS request
+    let request = setRequestProxy (Just (Proxy "45.151.169.28" 80)) botURL
+    res <- httpLBS request
     return (getResponseBody res)
 
 
@@ -20,5 +25,5 @@ main :: IO ()
 main = do
   putStrLn "hello world"
   json <- fetchJSON
-  BS.putStrLn json
+  L8.putStrLn json
 
