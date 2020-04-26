d75c3c8e5685da4774d4a36c919809314d0ed815
urbanizzz <yuraoskin@gmail.com>
2020-04-27 01:03:02 +0700

    Done writeCfg and sendMsg
    Changed type of lastMsg

--- src/Main.hs
+++ src/Main.hs
@@ -16,8 +16,8 @@ import            Control.Applicative         ((<$>), (<*>))
 -- config
 data Config = Config {
   about           :: String,
-  repeat          :: Integer,
-  repeat_question :: String,
+  repeatNumber    :: Integer,
+  repeatQuestion  :: String,
   offset          :: Integer }
 instance FromJSON Config where
   parseJSON (Object cfg)  = Config
@@ -63,24 +63,26 @@ data Messages = Messages [Message]
 instance FromJSON Messages where
   parseJSON (Object msgs) = Messages <$> msgs .: "result"
 
-botURL = "https://api.telegram.org/bot949284451:AAGK8fCgIhv2KLcmT8Mz_bf-3hAl0Ccp7pA/getUpdates?offset=185794573"
-botURL1 = "https://api.telegram.org/bot949284451:AAGK8fCgIhv2KLcmT8Mz_bf-3hAl0Ccp7pA/sendMessage?chat_id=845633894&text=fuck you"
+botURL = "https://api.telegram.org/bot949284451:AAGK8fCgIhv2KLcmT8Mz_bf-3hAl0Ccp7pA/"
 fileCfg = "config.json"
+defaultCfg  = Config {
+                about = "UrbanizzzBot",
+                repeatNumber = 1,
+                repeatQuestion = "Enter the number of repetitions",
+                offset = 0}
 
 readCfg :: IO Config
 readCfg = do
   rawJSON <- B.readFile fileCfg
   let result = decode rawJSON :: Maybe Config
   return $ case result of
-    Nothing -> Config "UrbanizzzBot" 0 "Enter the number of repetitions" 0
+    Nothing -> defaultCfg
     Just js -> js
 
-{-
+--todo bracketsOnError
 writeCfg :: Config -> IO ()
 writeCfg cfg = withFile fileCfg WriteMode (\handle -> do
-  data <- encode cfg
-  B8.hPutStr data)
--}
+  B8.hPutStr handle $ Pr.encodePretty cfg)
 {-
   bracketOnError
     (openTemplFile "." "temp")
@@ -90,23 +92,25 @@ writeCfg cfg = withFile fileCfg WriteMode (\handle -> do
     (\(tempName, tempHandle) -> do
       B.hPutStr tempHandle cfg
       hClose tempHandle
-      renameFile tempName fileCfg)
--}
+      renameFile tempName fileCfg)-}
+
 
 fetchJSON :: IO B.ByteString
 fetchJSON = do
-  let request = setRequestProxy (Just (Proxy "127.0.0.1" 9041)) $ botURL
+  cfg <- readCfg
+  url <- parseRequest $ botURL ++ "getUpdates?offset=" ++ (show . offset $ cfg)
+  let request = setRequestProxy (Just (Proxy "127.0.0.1" 9041)) url
   res <- httpLBS request
   return (getResponseBody res)
 
 -- getting last message
-lastMsg :: IO String
+lastMsg :: IO (Maybe Message)
 lastMsg = do
   rawJSON <- fetchJSON
   let result = decode rawJSON :: Maybe Messages
   return $ case result of
-    Nothing -> "Error"
-    Just (Messages js) -> if null js then "" else printPretty . last $ js
+    Nothing -> Nothing
+    Just (Messages js) -> if null js then Nothing else Just . last $ js
 
 -- getting list of messages
 listMsg :: IO String
@@ -117,21 +121,26 @@ listMsg = do
     Nothing -> "Error"
     Just (Messages js) -> foldl (\a s -> a ++ "\n" ++ s) "" $ printPretty <$> js
 
-sendMsg :: IO ()
-sendMsg = do
-  let request = setRequestProxy (Just (Proxy "127.0.0.1" 9041)) $ botURL1
+{-
+repeatMsg :: IO ()
+repeatMsg = do
+  return $ case lastMsg of
+    Nothing -> ()
+    Just msg -> sendMsg msg
+-}
+
+-- sending message
+sendMsg :: Message -> IO ()
+sendMsg msg = do
+  url <- parseRequest $ botURL ++ "sendMessage?chat_id=" ++ (show . chat_id $ msg) ++ "&text=" ++ (text msg)
+  let request = setRequestProxy (Just (Proxy "127.0.0.1" 9041)) $ url
   httpLBS request
   return ()
 
 main :: IO ()
 main = do
-  -- listMsg or lastMsg
-  msg <- listMsg
-  putStrLn msg
-  cfg <- readCfg
-  putStrLn $ show cfg
-  let cfg1 = Config "qwe" 1 "asd" 0
-    in B8.putStrLn $ encode cfg1
+  msg <- lastMsg
+  return ()
 
 
 
